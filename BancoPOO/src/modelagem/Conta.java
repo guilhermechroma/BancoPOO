af85a837947e81251233ab0ad735122e1e51cf18
ChromaCaipira <guilherme34956@gmail.com>
2022-04-12 14:42:33 -0300

    Criação do Projeto

---
+++ BancoPOO/src/modelagem/Conta.java
@@ -0,0 +1,37 @@
+package modelagem;
+
+public class Conta {
+	private String numero;
+	private String agencia;
+	private String digito;
+	private String cliente;
+	private double saldo;
+	
+	public Conta(String numero, String agencia, String digito, String cliente, double saldo) {
+		this.numero = numero;
+		this.agencia = agencia;
+		this.digito = digito;
+		this.cliente = cliente;
+		this.saldo = saldo;
+	}
+	
+	public String getNumero() { //Número
+		return this.numero;
+	}
+	
+	public String getAgencia() { //Agência
+		return this.agencia;
+	}
+
+	public String getDigito() { //Digito
+		return this.digito;
+	}
+
+	public String getCliente() { //Cliente
+		return this.cliente;
+	}
+
+	public double getSaldo() { //Saldo
+		return this.saldo;
+	}
+}
